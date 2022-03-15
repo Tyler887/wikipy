5238b147f17326ec9409c3bbe850725214712974
Tyler887 <tylermageeshields@gmail.com>
2022-03-15 23:38:44 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ try:
   import django
 except ImportError:
   print("Error: Could not locate Djano. Make sure it's in your PYTHONPATH.")
-# more work to be done soon, see issues
+# more work to be done soon, see https://github.com/Tyler887/wikipy/issues?q=is%3Aopen+is%3Aissue+label%3Atodo
